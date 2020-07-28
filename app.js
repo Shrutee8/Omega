81725fbb73a7fe7c1efca7876e271e039f235312
Shrutee8 <68885876+Shrutee8@users.noreply.github.com>
2020-07-28 09:47:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyC9mH90nvlHBgE2X6_uTjTrLmuutRCCARY",
+  authDomain: "shruti-1403a.firebaseapp.com",
+  databaseURL: "https://shruti-1403a.firebaseio.com",
+  projectId: "shruti-1403a",
+  storageBucket: "shruti-1403a.appspot.com",
+  messagingSenderId: "320645640339",
+  appId: "1:320645640339:web:cce35e380cabbb49d45d66",
+  measurementId: "G-ZP7N0T88FG"
 };
 
      
